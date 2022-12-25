42becb1ec1b200788cde6c530de606b885b4928a
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2022-12-24 23:54:14 -0300

    Add font Roboto and Roboto mono

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './styles/global';
 import { defaultTheme } from './styles/themes/defaultTheme';
 
 export const App = () => {
@@ -7,6 +8,7 @@ export const App = () => {
       <div className="App">
         <h1>test</h1>
       </div>
+      <GlobalStyles />
     </ThemeProvider>
   );
 };
