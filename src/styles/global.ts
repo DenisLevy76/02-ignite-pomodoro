e44177f45f0dfe132e0a01383ab44c7cdb0c839b
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2022-12-25 00:00:35 -0300

    Add default theme

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,14 +1,29 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+  ${({ theme }) => css`
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+    :focus {
+      outline: none;
+      box-shadow: 0 0 0 2px ${theme.colors['green-500']};
+    }
 
-  body {
-    background: #333;
-    color: #fff;
-  }
+    body {
+      background: ${theme.colors['gray-900']};
+      color: ${theme.colors['gray-300']};
+    }
+
+    body,
+    input,
+    textarea,
+    button {
+      font-family: ${theme.type.fontFamily};
+      font-weight: 400;
+      font-size: 1rem;
+    }
+  `}
 `;
